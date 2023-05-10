9b919f07b17f917bc05724471d93d9e18888202e
otavioteixeira27 <125220121+otavioteixeira27@users.noreply.github.com>
2023-05-10 17:11:24 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -34,3 +34,6 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+
+date = st.date_input("Pick a date")
