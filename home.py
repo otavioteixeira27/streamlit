7a73ca4200ac8ee99a99eb14ba66ceb394ab6837
otavioteixeira27 <125220121+otavioteixeira27@users.noreply.github.com>
2023-05-10 17:33:39 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,5 @@
 streamlit run [entrypoint file]
 
-home.py # This is the file you run with "streamlit run"
+dash.py # This is the file you run with "streamlit run"
 └─── pages/
-  └─── dash.py # This is a page
-  └─── dash2.py # This is another page
+  └─── dash2.py # This is a page
