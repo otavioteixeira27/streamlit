5faea8b985cc550806aa9499691791a0a5a46a81
otavioteixeira27 <125220121+otavioteixeira27@users.noreply.github.com>
2023-05-10 16:45:22 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -4,4 +4,4 @@ import numpy as np
 
 st.title('Uber pickups in NYC')
 
-streamlit run uber_pickups.py
+streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/streamlit_app.py
