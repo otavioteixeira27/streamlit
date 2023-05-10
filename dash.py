276c35ad876304954f1684d372c5b2e60e8cc5ec
otavioteixeira27 <125220121+otavioteixeira27@users.noreply.github.com>
2023-05-10 16:48:38 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -5,6 +5,6 @@ import numpy as np
 st.subheader('Number of pickups by hour')
 
 hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    data[DATE_COLUMN].dt.hour, range=(0,24))[0]
 
 st.bar_chart(hist_values)
