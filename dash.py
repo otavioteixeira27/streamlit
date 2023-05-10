4ed86a4c03fe5d8bf68378387d82e07c0e7ef012
otavioteixeira27 <125220121+otavioteixeira27@users.noreply.github.com>
2023-05-10 16:47:46 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,15 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.subheader('Number of pickups by hour')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+st.bar_chart(hist_values)
